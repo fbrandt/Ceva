971209a43cf941e1ead321c7ccf310397c25a4f8
Felix Brandt <brandt@fzi.de>
2018-02-14 18:31:33 +1100

    FEATURE: add Builder class to turn InstanceFilterConfiguration in HQLFilter

---
+++ src/main/java/de/felixbrandt/ceva/InstanceFilterBuilder.java
@@ -0,0 +1,51 @@
+package de.felixbrandt.ceva;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.felixbrandt.ceva.config.InstanceFilterConfiguration;
+import de.felixbrandt.ceva.config.MetricValueInstanceFilterConfiguration;
+import de.felixbrandt.ceva.entity.InstanceMetric;
+import de.felixbrandt.ceva.provider.HQLFilter;
+import de.felixbrandt.ceva.provider.InstanceMetricDBProvider;
+import de.felixbrandt.ceva.provider.InstanceMetricValueHQLFilter;
+
+/**
+ * Build HQLFilters according to InstanceFilterConfiguration.
+ */
+public class InstanceFilterBuilder
+{
+  private InstanceMetricDBProvider instance_metric_provider;
+
+  public InstanceFilterBuilder(InstanceMetricDBProvider provider)
+  {
+    instance_metric_provider = provider;
+  }
+
+  public List<HQLFilter> build (List<InstanceFilterConfiguration> configs)
+  {
+    List<HQLFilter> filters = new ArrayList<HQLFilter>();
+
+    for (InstanceFilterConfiguration config : configs) {
+      filters.add(build(config));
+    }
+
+    return filters;
+  }
+
+  public HQLFilter build (InstanceFilterConfiguration config)
+  {
+    if (config.getType().equals("metricvalue")) {
+      return buildValueFilter((MetricValueInstanceFilterConfiguration) config);
+    }
+    return null;
+  }
+
+  private InstanceMetricValueHQLFilter buildValueFilter (
+          MetricValueInstanceFilterConfiguration config)
+  {
+    InstanceMetric metric = instance_metric_provider.findByName(config.getMetric()).iterator()
+            .next();
+    return new InstanceMetricValueHQLFilter(metric, config.getValues());
+  }
+}
